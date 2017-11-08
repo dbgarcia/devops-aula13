7cb65c4b7da4c8251e2c28232d076615e85d299f
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-08 20:36:05 -0200

    Criação da classe Candidato

--- app/models.py
+++ app/models.py
@@ -13,3 +13,9 @@ class Curso(models.Model):
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
 
+class Candidato(models.Model):
+    nome = models.CharField(max_length=200)
+    rg = models.CharField(max_length=20)
+    cpf = models.CharField(max_length=13)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=15)
