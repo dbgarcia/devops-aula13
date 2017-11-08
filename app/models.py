d2b7b36a6af6ebfd18d8990560b72a0cd4298ba8
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-08 20:36:33 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,10 +12,3 @@ class Curso(models.Model):
 
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
-
-class Candidato(models.Model):
-    nome = models.CharField(max_length=200)
-    rg = models.CharField(max_length=20)
-    cpf = models.CharField(max_length=13)
-    endereco = models.CharField(max_length=200)
-    telefone = models.CharField(max_length=15)
